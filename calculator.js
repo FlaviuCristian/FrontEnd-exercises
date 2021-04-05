43b700f4bc641c3a7f02f881c2aa5397b2f1497c
FlaviuCristian <flaviucristian@gmail.com>
2021-04-05 20:47:44 +0300

    initial commit for the calculator

---
+++ calculator.js
@@ -0,0 +1,85 @@
+let runninTotal = 0;
+let buffer = "0";
+let previousOperator = null;
+const screen = document.querySelector(".screen")
+
+document
+    .querySelector('.calc-buttons')
+    .addEventListener("click", function(event) {
+        buttonClick(event.target.innerText);
+    })
+
+function buttonClick(value) {
+    if (isNaN(parseInt(value))) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    rerender();
+}
+
+function handleNumber(value) {
+    if (buffer === "0") {
+        buffer = value;
+    } else {
+        buffer += value;
+    }
+}
+
+function handleSymbol(value) {
+    switch (value) {
+        case 'C':
+            buffer = "0";
+            runninTotal = 0;
+            previousOperator = null
+        case '=':
+            if (previousOperator === null) {
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = " " + runninTotal;
+            runninTotal = 0;
+            break;
+
+        case "←":
+            if (buffer.length === 1) {
+                buffer = "0";
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        default:
+            handleMath(value);
+            break;
+    }
+}
+
+function handleMath(value) {
+    const intBuffer = parseInt(buffer);
+    if (runninTotal === 0) {
+        runninTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = value;
+
+    buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+    if (previousOperator === "+") {
+        runninTotal += intBuffer;
+    } else if (previousOperator === "-") {
+        runninTotal -= intBuffer;
+    } else if (previousOperator === "x") {
+        runninTotal *= intBuffer;
+    } else {
+        runninTotal /= intBuffer;
+    }
+}
+
+function rerender() {
+    screen.innerText = buffer;
+}
